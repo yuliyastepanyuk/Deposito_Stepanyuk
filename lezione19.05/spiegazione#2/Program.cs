deb0d2810c3e044b7f52f7ac3d04d3feab1c70b4
yuliya <84444059+yuliyastepanyuk@users.noreply.github.com>
2025-05-19 16:42:43 +0200

    Add files via upload

---
+++ lezione19.05/spiegazione#2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Persona
+{
+    public string Nome;
+    public int Eta;
+
+    //Construttore con parametri
+    public Persona(string nome, int eta)
+    {
+        Nome = nome;
+        Eta = eta;
+    }
+
+    public void Presentati()
+    {
+        Console.WriteLine($"Ciao, sono {Nome} e ho {Eta} anni.");
+    }
+}
+
+public class Programma()
+{
+    public static void Main()
+    {
+        //Creazione dell'oggetto con costruttore
+        Persona p = new Persona("Anna", 30);
+
+        //Chiamata al metodo
+        p.Presentati(); // chiamata al metodo Presentati dell'oggetto p
+    }
+}
