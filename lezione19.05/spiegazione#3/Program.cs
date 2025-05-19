5a0549c6027a700e73b865d12af60382427770c6
yuliya <84444059+yuliyastepanyuk@users.noreply.github.com>
2025-05-19 17:36:15 +0200

    Add files via upload

---
+++ lezione19.05/spiegazione#3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Persona
+{
+    public string Nome;
+    public int Eta;
+
+    // Sovrascirve il metodo ToString per personalizare l'output
+    public override string ToString()
+    {
+        return $"Nome: {Nome}, Età: {Eta}";
+    }
+}
+
+public class Programma
+{
+    public static void Main()
+    {
+        Persona p = new Persona { Nome = "Mario", Eta = 25 };
+        Console.WriteLine(p); // Stampa l'oggetto Persona
+    }
+}
