addb36e8310efd3f37b6dc62629c13e0b539923f
yuliya <84444059+yuliyastepanyuk@users.noreply.github.com>
2025-07-16 10:58:06 +0200

    Add files via upload

---
+++ lezione05.06/esercizi#enum/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+public enum GiorniSettimana
+{
+    Lunedì,
+    Martedì,
+    Mercoledì,
+    Giovedì,
+    Venerdì,
+    Sabato,
+    Domenica
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        GiorniSettimana giorno;
+
+        Console.WriteLine("Scegli un giorno della settimana:");
+        foreach (GiorniSettimana g in Enum.GetValues(typeof(GiorniSettimana)))
+        {
+            Console.WriteLine(g);
+        }
+        string input = Console.ReadLine();
+
+        switch(input)
+        {
+            case "Lunedì":
+                giorno = GiorniSettimana.Lunedì;
+                Console.WriteLine("Hai scelto Lunedì.");
+                break;
+            case "Martedì":
+                giorno = GiorniSettimana.Martedì;
+                Console.WriteLine("Hai scelto Martedì.");
+                break;
+            case "Mercoledì":
+                giorno = GiorniSettimana.Mercoledì;
+                Console.WriteLine("Hai scelto Mercoledì.");
+                break;
+            case "Giovedì":
+                giorno = GiorniSettimana.Giovedì;
+                Console.WriteLine("Hai scelto Giovedì.");
+                break;
+            case "Venerdì":
+                giorno = GiorniSettimana.Venerdì;
+                Console.WriteLine("Hai scelto Venerdì.");
+                break;
+            case "Sabato":
+                giorno = GiorniSettimana.Sabato;
+                Console.WriteLine("Hai scelto Sabato.");
+                break;
+            case "Domenica":
+                giorno = GiorniSettimana.Domenica;
+                Console.WriteLine("Hai scelto Domenica.");
+                break;
+            default:
+                Console.WriteLine("Giorno non valido.");
+                return;    
+        } 
+
+    }
+}
