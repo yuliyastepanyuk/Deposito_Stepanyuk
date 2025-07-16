0c4d4c07800bff01f028b233cc90dcf230155ec3
yuliya <84444059+yuliyastepanyuk@users.noreply.github.com>
2025-07-16 10:43:30 +0200

    Add files via upload

---
+++ Verifica#1#Mirko/esercizioContoCorrente/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.ComponentModel.Design.Serialization;
+using System.Reflection;
+
+public class ContoCorrente
+{
+    private decimal Saldo;
+    private int NumeroOperazioni;
+
+    public decimal saldo
+    {
+        get
+        {
+            return Saldo;
+        }
+    }
+
+    public int numeroOperazioni
+    {
+        get
+        {
+            return NumeroOperazioni;
+        }
+    }
+
+    public decimal Versa(decimal importo) // creo il metodo per far aggiornare il saldo all'utente del suo conto corrente
+    {
+        Console.WriteLine("Inserisci l'importo da versare: ");
+        importo = decimal.Parse(Console.ReadLine());
+        Saldo += importo;                           // aggiorna il campo privato
+        NumeroOperazioni++;                         // aggiorna il campo privato
+        Console.WriteLine($"Il tuo saldo è di: {Saldo}");
+        return Saldo;
+    }
+
+    public decimal Preleva(decimal importo)
+    {
+        if (Saldo >= importo)
+        {
+            Console.WriteLine("Quanto vuoi prelevare?");
+            importo = importo = decimal.Parse(Console.ReadLine());
+            Saldo -= importo;
+            NumeroOperazioni--;
+            Console.WriteLine($"Il tuo saldo è di: {Saldo}");
+            return Saldo;
+        }
+        else
+        {
+            Console.WriteLine("Non hai fondi sufficienti!");
+            return Saldo;
+        }
+    }
+}
+
+public class Programma
+{
+    public static void Main()
+    {
+        bool continua = true;
+     
+        do
+        {
+            Console.WriteLine("Benvenuto! Cosa vuoi fare: 1.Creare un conto corrente\n 2.Versare denaro\n 3.Prelevare denaro\n 5.Visulizzare il numero di operazioni effettuate\n 5.Visualizzare il saldo\n 6.Uscire");
+            int scelta = int.Parse(Console.ReadLine());
+            if (scelta >= 1 && scelta <= 5)
+            {
+                switch (scelta)
+                {
+                    case 1:
+                        ContoCorrente conto = new ContoCorrente(); // creo l'oggetto conto
+                        Console.WriteLine("Conto creato!");
+                        break;
+                    case 2:
+                        conto.Versa(0); // richiamo il metodo Versa
+                        break;
+                    case 3:
+                        conto.Preleva(0); // richiamo il metodo Preleva
+                        break;
+                    case 4:
+                         Console.WriteLine($"Numero operazioni: {conto.numeroOperazioni}");
+                        break;
+                    case 5:
+                        Console.WriteLine($"Saldo attuale: {conto.saldo}");
+                        break;    
+                    case 6:
+                        Console.WriteLine("Arrivederci!");
+                        continua = false;
+                        break;
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Seleziona un'operazione valida!");
+            }
+
+        }
+        while (continua);
+    }
+
+}
---
+++ Verifica#1#Mirko/esercizioVeicoli/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+public class Veicolo // classe padre
+{
+    public string Marca;
+    public string Modello;
+    public int AnnoImmatricolazione;
+
+    public Veicolo(string marca, string modello, int annoImmatricolazione) // il suo costruttore ovvero ciò che ne definisce la forma
+    {
+        Marca = marca;
+        Modello = modello;
+        AnnoImmatricolazione = annoImmatricolazione;
+    }
+
+    public virtual void StampaInfo()
+    {
+        Console.WriteLine($"Ecco le informazioni del veicolo: {Marca}\n {Modello}\n{AnnoImmatricolazione}\n ");
+    }
+}
+
+public class AutoAziendale : Veicolo // classe figlia
+{
+    string Targa;
+    bool UsoPrivato;
+
+    public AutoAziendale(string marca, string modello, int annoImmatricolazione, string targa, bool usoPrivato) : base(marca, modello, annoImmatricolazione) // il suo costruttore
+    {
+        Targa = targa;
+        UsoPrivato = usoPrivato;
+    }
+
+    public override void StampaInfo() // override del metodo per personalizzare il messaggio di ritnorno
+    {
+        base.StampaInfo();
+        Console.WriteLine($"{Targa}\n {UsoPrivato}");
+    }
+}
+
+public class FurgoneAziendale : Veicolo // classe dereivata
+{
+    int CapacitaCarico;
+
+    public FurgoneAziendale(string marca, string modello, int annoImmatricolazione, int capacitaCarico) : base(marca, modello, annoImmatricolazione) // il suo costruttore
+    {
+        CapacitaCarico = capacitaCarico;
+    }
+
+    public override void StampaInfo() // override del metodo per personalizzare il messaggio di ritnorno
+    {
+        base.StampaInfo();
+        Console.WriteLine($"{CapacitaCarico}");
+    }
+}
+
+public class Programma
+{
+    public static void Main()
+    {
+        List<Veicolo> veicoli = new List<Veicolo>
+        {
+            new Veicolo("Fiat", "Panda", 2010),
+            new AutoAziendale("Volkswagen", "Golf", 2018, "AB123CD", true),
+            new FurgoneAziendale("Ford", "Transit", 2015, 1200)
+        };
+
+        foreach (Veicolo v in veicoli)
+        {
+            v.StampaInfo();
+        }
+    }
+}
